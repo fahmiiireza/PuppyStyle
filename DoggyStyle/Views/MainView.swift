0d5f5d22a80702afca1313bf5d47314187c6af66
felixparey <mysteryparey@gmail.com>
2023-12-07 23:18:10 +0100

    Added some functionalities to LogIn View and made it accessible through Search View on Profile Image

--- DoggyStyle/Views/MainView.swift
+++ DoggyStyle/Views/MainView.swift
@@ -18,7 +18,7 @@ struct MainView: View {
             TabView{
                 MyDogsView(backgroundLogic: backgroundLogic)
                     .tabItem { Label("My Dogs", systemImage: "dog.fill") }
-                SearchView()
+                SearchView(backroundLogic: backgroundLogic)
                     .tabItem { Label("Search", systemImage: "magnifyingglass") }
             }
         }else{
--- DoggyStyle/Views/SignUpView.swift
+++ DoggyStyle/Views/SignUpView.swift
@@ -7,9 +7,14 @@
 
 import SwiftUI
 
+enum FocusedField {
+        case mail, password, confirmedPassword
+    }
 
 struct SignUpView: View {
     
+    @FocusState private var focusField: FocusedField?
+    @Environment(\.dismiss) private var dismiss
     @State private var mail = ""
     @State private var password = ""
     @State private var confirmedPassword = ""
@@ -17,168 +22,210 @@ struct SignUpView: View {
     
     var body: some View {
         
-        VStack {
-            
-            HStack {
+        NavigationStack {
+            VStack {
                 
-                Button(action: {
-                    signingUp = true
-                }, label: {
-                    Text("Sign Up")
-                        .foregroundStyle(signingUp ? Color.primary : Color.gray)
+                HStack {
+                    
+                    Button(action: {
+                        signingUp = true
+                    }, label: {
+                        Text("Sign Up")
+                            .foregroundStyle(signingUp ? Color.primary : Color.gray)
+                            .font(.title)
+                            .bold()
+                    })
+                    
+                    Text("|")
                         .font(.title)
                         .bold()
-                })
+                    
+                    Button(action: {
+                        signingUp = false
+                    }, label: {
+                        Text("Log In")
+                            .foregroundStyle(signingUp ? Color.gray : Color.primary)
+                            .font(.title)
+                            .bold()
+                    })
+                    
+                }
                 
-                Text("|")
-                    .font(.title)
+                Image("Appicon")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .containerRelativeFrame(.vertical){size, axis  in
+                        size * 0.2
+                    }
+                    .clipShape(Circle())
+                
+                Text("Doggy Style")
+                    .font(.largeTitle)
                     .bold()
                 
-                Button(action: {
-                    signingUp = false
-                }, label: {
-                    Text("Log In")
-                        .foregroundStyle(signingUp ? Color.gray : Color.primary)
-                        .font(.title)
-                        .bold()
-                })
+                Divider()
                 
-            }
-            
-            Image("Appicon")
-                .resizable()
-                .aspectRatio(contentMode: .fit)
-                .containerRelativeFrame(.vertical){size, axis  in
-                    size * 0.2
-                }
-                .clipShape(Circle())
-            
-            Text("Doggy Style")
-                .font(.largeTitle)
-                .bold()
-            
-            Divider()
-            
-            HStack{
-                VStack(alignment: .leading){
-                    
-                    if signingUp{
-                        Text("Sign up with E-Mail")
-                            .font(.headline)
-                    }else{
-                        Text("Log in with E-Mail")
-                            .font(.headline)
-                    }
-                    
-                    TextField("E-Mail", text: $mail)
-                        .keyboardType(.emailAddress)
-                        .textFieldStyle(.plain)
-                        .padding(10)
-                        .background(.gray.opacity(0.3))
-                        .clipShape(RoundedRectangle(cornerRadius: 10))
+                HStack{
+                    VStack(alignment: .leading){
                         
-                    SecureField("Password", text: $password)
+                        if signingUp{
+                            Text("Sign up with E-Mail")
+                                .font(.headline)
+                        }else{
+                            Text("Log in with E-Mail")
+                                .font(.headline)
+                        }
                         
-                        .textFieldStyle(.plain)
-                        .padding(10)
-                        .background(.gray.opacity(0.3))
-                        .clipShape(RoundedRectangle(cornerRadius: 10))
-                    
-                    if signingUp{
-                        SecureField("Confirm Password", text: $confirmedPassword)
+                        TextField("E-Mail", text: $mail)
+                            .keyboardType(.emailAddress)
                             .textFieldStyle(.plain)
                             .padding(10)
                             .background(.gray.opacity(0.3))
                             .clipShape(RoundedRectangle(cornerRadius: 10))
-                    }
-                    
-                    HStack{
-                        Spacer()
-                        Text("OR")
-                        Spacer()
-                    }
-                    
-                    //Sign in with Google Button
-                    if signingUp {
-                        Button(action: {
-                            
-                        }, label: {
-                            ZStack {
-                                RoundedRectangle(cornerRadius: 10)
-                                    .foregroundStyle(.white)
-                                    .frame(height: 50)
-                                    .shadow(color: .gray.opacity(0.4), radius: 5)
-                                    
-                                HStack{
-                                    Image("Google")
-                                        .resizable()
-                                        .aspectRatio(contentMode: .fit)
-                                    Text("Sign up with Google")
-                                        .font(.title3)
-                                        
-                                        .foregroundStyle(.black)
-                                    Spacer()
-                                }
-                                .padding(10)
-                                .frame(height: 50)
+                            .autocorrectionDisabled()
+                            .focused($focusField, equals: .mail)
+                            .onSubmit() {
+                                focusField = .password
                             }
-                        })
-                    }else{
-                        Button(action: {
                             
-                        }, label: {
-                            ZStack {
-                                RoundedRectangle(cornerRadius: 10)
-                                    .foregroundStyle(.white)
-                                    .frame(height: 50)
-                                    .shadow(color: .gray.opacity(0.4), radius: 5)
-                                    
-                                HStack{
-                                    Image("Google")
-                                        .resizable()
-                                        .aspectRatio(contentMode: .fit)
-                                    Text("Sign In with Google")
-                                        .font(.title3)
-                                        
-                                        .foregroundStyle(.black)
-                                    Spacer()
+                        SecureField("Password", text: $password)
+                            
+                            .textFieldStyle(.plain)
+                            .padding(10)
+                            .background(.gray.opacity(0.3))
+                            .clipShape(RoundedRectangle(cornerRadius: 10))
+                            .focused($focusField, equals: .password)
+                            .autocorrectionDisabled()
+                            .onSubmit() {
+                                if signingUp{
+                                    focusField = .confirmedPassword
+                                }else{
+                                    //check if correct and sign in
+                                    print("Signed In")
+                                    dismiss.callAsFunction()
                                 }
-                                .padding(10)
-                                .frame(height: 50)
                             }
-                        })
+                        
+                        if signingUp{
+                            SecureField("Confirm Password", text: $confirmedPassword)
+                                .textFieldStyle(.plain)
+                                .padding(10)
+                                .background(.gray.opacity(0.3))
+                                .clipShape(RoundedRectangle(cornerRadius: 10))
+                                .focused($focusField, equals: .confirmedPassword)
+                                .autocorrectionDisabled()
+                                .onSubmit() {
+                                    //check if correct and sign up
+                                    print("Signed Up")
+                                    dismiss.callAsFunction()
+                                }
+                        }
+                        
+                        HStack{
+                            Spacer()
+                            Text("OR")
+                            Spacer()
+                        }
+                        
+                        //Sign in with Google Button
+                        if signingUp {
+                            Button(action: {
+                                
+                            }, label: {
+                                ZStack {
+                                    RoundedRectangle(cornerRadius: 10)
+                                        .foregroundStyle(.white)
+                                        .frame(height: 50)
+                                        .shadow(color: .gray.opacity(0.4), radius: 5)
+                                        
+                                    HStack{
+                                        Image("Google")
+                                            .resizable()
+                                            .aspectRatio(contentMode: .fit)
+                                        Text("Sign up with Google")
+                                            .font(.title3)
+                                            
+                                            .foregroundStyle(.black)
+                                        Spacer()
+                                    }
+                                    .padding(10)
+                                    .frame(height: 50)
+                                }
+                            })
+                        }else{
+                            Button(action: {
+                                
+                            }, label: {
+                                ZStack {
+                                    RoundedRectangle(cornerRadius: 10)
+                                        .foregroundStyle(.white)
+                                        .frame(height: 50)
+                                        .shadow(color: .gray.opacity(0.4), radius: 5)
+                                        
+                                    HStack{
+                                        Image("Google")
+                                            .resizable()
+                                            .aspectRatio(contentMode: .fit)
+                                        Text("Sign In with Google")
+                                            .font(.title3)
+                                            
+                                            .foregroundStyle(.black)
+                                        Spacer()
+                                    }
+                                    .padding(10)
+                                    .frame(height: 50)
+                                }
+                            })
+                        }
+                        
+                        
+                        
                     }
-                    
-                    
-                    
+                    .padding()
+                    Spacer()
                 }
-                .padding()
                 Spacer()
+                
+                if signingUp{
+                    Text("Already have an account?")
+                    
+                    Button(action: {
+                        signingUp = false
+                    }, label: {
+                        Text("Sign In")
+                            .underline()
+                    })
+                }else{
+                    Text("Don't have an account yet?")
+                    
+                    Button(action: {
+                        signingUp = true
+                    }, label: {
+                        Text("Sign Up")
+                            .underline()
+                    })
+                }
+                
+                
+                    
             }
-            Spacer()
-            
-            if signingUp{
-                Text("Already have an account?")
-                
-                Button(action: {
-                    signingUp = false
-                }, label: {
-                    Text("Sign In")
-                        .underline()
-                })
-            }else{
-                Text("Don't have an account yet?")
-                
-                Button(action: {
-                    signingUp = true
-                }, label: {
-                    Text("Sign Up")
-                        .underline()
-                })
-            }
-            
-            
-                
+            .toolbar(content: {
+                ToolbarItem(placement: .cancellationAction) {
+                    Button("Cancel") {
+                        dismiss.callAsFunction()
+                    }
+                }
+                ToolbarItem(placement: .primaryAction) {
+                    Button {
+                        dismiss.callAsFunction()
+                    }label: {
+                        Text(signingUp ? "Sign Up" : "Sign In")
+                            .bold()
+                            .disabled(mail.isEmpty || password.isEmpty || confirmedPassword.isEmpty)
+                    }
+                }
+            })
+            .padding(.top)
         }
         
     }
