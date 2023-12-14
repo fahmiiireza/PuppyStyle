7b765c432089dcc138000e8771c6a5d21796269f
felixparey <mysteryparey@gmail.com>
2023-12-14 01:05:28 +0100

    Added BG Image to tinder but it doesnt update when we go back

--- DoggyStyle/Views/TinderCardView.swift
+++ DoggyStyle/Views/TinderCardView.swift
@@ -12,6 +12,7 @@ struct TinderCardView: View {
     @Environment(BackgroundLogic.self) private var backgroundLogic
     var dog: Dog
    var breeds = ["Golden Retriever", "Border Collie", "Boxer"]
+    let urls = [URL(string: "https://hips.hearstapps.com/hmg-prod/images/dog-puppy-on-garden-royalty-free-image-1586966191.jpg?crop=0.752xw:1.00xh;0.175xw,0&resize=1200:*")!, URL(string: "https://www.thesprucepets.com/thmb/y4YEErOurgco9QQO-zJ6Ld1yVkQ=/3000x0/filters:no_upscale():strip_icc()/english-dog-breeds-4788340-hero-14a64cf053ca40f78e5bd078b052d97f.jpg")!, URL(string: "https://hips.hearstapps.com/hmg-prod/images/closeup-of-a-black-russian-tsvetnaya-bolonka-royalty-free-image-1681161235.jpg?crop=0.563xw:1.00xh;0.204xw,0&resize=1200:*")!]
     
     var body: some View {
         
@@ -21,8 +22,9 @@ struct TinderCardView: View {
                     Text(dog.name)
                         .font(.title)
                     .bold()
+                    .padding(.horizontal, 5)
                        
-                    AsyncImage(url: URL(string: "https://hips.hearstapps.com/hmg-prod/images/dog-puppy-on-garden-royalty-free-image-1586966191.jpg?crop=0.752xw:1.00xh;0.175xw,0&resize=1200:*")) { Image in
+                AsyncImage(url: urls.randomElement()) { Image in
                         Image.resizable()
                             .aspectRatio(contentMode: .fill)
                             .containerRelativeFrame(.vertical, { size, _ in
@@ -33,7 +35,7 @@ struct TinderCardView: View {
                             })
                             .clipShape(RoundedRectangle(cornerRadius: 20))
                             .onAppear(perform: {
-                                withAnimation {
+                                withAnimation(.linear(duration: 0.2)) {
                                     backgroundLogic.backgroundImage = Image
                                 }
                                 
@@ -47,7 +49,7 @@ struct TinderCardView: View {
                 Text(breeds.joined(separator: " ‧ "))
                     .foregroundStyle(.white.opacity(0.8))
                     .font(.headline)
-                    .padding()
+                    .padding(5)
                 }
                 
             Spacer()
