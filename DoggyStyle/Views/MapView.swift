451e8f57980b497b0f5c16259b025440bb38f3bf
Fahmi Fahreza <fahmiiireza@gmail.com>
2023-12-15 08:21:17 +0100

    added CLLocation to get users location

--- DoggyStyle/Views/MapView.swift
+++ DoggyStyle/Views/MapView.swift
@@ -10,41 +10,121 @@ import MapKit
 
 extension CLLocationCoordinate2D {
    static let parking = CLLocationCoordinate2D(latitude: 40.8522, longitude: 14.2681)
+    
 }
-struct City: Identifiable {
-    let id = UUID()
-    let name: String
-    let coordinate: CLLocationCoordinate2D
+extension MKCoordinateRegion {
+    static let napoli = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 40.853200, longitude: 14.278025), span: MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1)
+    )
 }
 
 struct MapView: View {
+    @StateObject private var viewModel = MapViewModel()
     @State private var places = PlaceList()
     @State private var searchResults: [MKMapItem] = []
-    @State private var position: MapCameraPosition = .userLocation(fallback: .automatic)
-    
+    @State private var position: MapCameraPosition = .automatic
+    @State private var selectedResult : Int?
+    @State private var counter = 1
     var body: some View {
-//        position.followsUserLocation == true
         Map(position: $position){
+            UserAnnotation()
             ForEach(places.places, id: \.self) { coordinate in
-                if coordinate.city == "New York" { //We suppose to change it to user location
+                if coordinate.city == "Napoli" { //We suppose to change it to user location
                     Annotation("Parking", coordinate: CLLocationCoordinate2D(latitude: coordinate.latitude, longitude: coordinate.longitude)) {
                         ZStack {
                             Image(coordinate.img)
                                 .resizable()
                                 .frame(width: 40,height: 40)
                                 .padding(5)
-                            
+//                                .tag(counter)
                         }
-                    }.annotationTitles(.hidden)
+//                        .onAppear {
+//                            addCounter()
+//                        }
+                    }
+//                    .annotationTitles(.hidden)
                 }
                     
-
+                
             }
-            
+        }.task {
+            await viewModel.checkIfLocationServiceIsEnabled()
         }
+    }
+    private func addCounter() {
+        counter += 1
     }
 }
 
 //#Preview {
-//    MapView(position: positionn)
+//    MapView(position: )
 //}
+final class MapViewModel: NSObject, ObservableObject, CLLocationManagerDelegate {
+    var locationManager : CLLocationManager?
+    
+    func checkIfLocationServiceIsEnabled() async {
+        print(CLLocationManager.locationServicesEnabled())
+        if CLLocationManager.locationServicesEnabled() {
+            locationManager = CLLocationManager()
+            locationManager!.delegate = self
+            checkLocationAuthorization()
+        } else {
+            print("We need their location")
+        }
+    }
+    
+    private func checkLocationAuthorization() {
+        guard let locationManager = locationManager else {
+            return
+        }
+        guard let location = locationManager.location?.coordinate else {
+            return
+        }
+        print(locationManager.location)
+        print(location)
+        let geoCoder = CLGeocoder()
+        let locationCity = CLLocation(latitude: location.latitude, longitude: location.longitude)
+        geoCoder.reverseGeocodeLocation(locationCity, completionHandler:
+            {
+                placemarks, error -> Void in
+
+                // Place details
+                guard let placeMark = placemarks?.first else { return }
+
+                // Location name
+                if let locationName = placeMark.location {
+//                    print(locationName)
+                }
+                // Street address
+                if let street = placeMark.thoroughfare {
+//                    print(street)
+                }
+                // City
+                if let city = placeMark.subAdministrativeArea {
+                    print(city)
+                }
+                // Zip code
+                if let zip = placeMark.isoCountryCode {
+//                    print(zip)
+                }
+                // Country
+                if let country = placeMark.country {
+//                    print(country)
+                }
+        })
+        switch locationManager.authorizationStatus {
+        case .notDetermined:
+            locationManager.requestWhenInUseAuthorization()
+        case .restricted:
+            print("show alert they're restricted via parental control")
+        case .denied:
+            print("allow it man")
+        case .authorizedAlways, .authorizedWhenInUse:
+            break
+        @unknown default:
+            break
+        }
+    }
+    func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
+        checkLocationAuthorization()
+    }
+}
